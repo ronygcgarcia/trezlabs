037008ec547dc225be87f676e0154cc7831038e2
Rony Garcia <ronygcgarcia@gmail.com>
2023-01-25 20:56:01 -0600

    feat: adds functionality for create book individually and by list

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/users', [UserController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/prueba', [UserController::class, 'prueba']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/books', [BookController::class, 'store']);
+    
+    Route::post('/books/list', [BookController::class, 'storeBookList']);
+}); 

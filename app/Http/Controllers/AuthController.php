9df6518806758344f81cbee8956dd9e115e42a3e
Rony Garcia <ronygcgarcia@gmail.com>
2023-01-25 19:34:24 -0600

    feat: adds API swagger documentation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,10 +11,49 @@ use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     *   tags={"Auth"},
+     *   path="/login",
+     *   @OA\RequestBody(
+     *      required=true,
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *              @OA\Property(
+     *                  property="email",
+     *                  description="User email",
+     *                  type="string",
+     *              ),
+     *              @OA\Property(
+     *                  property="password",
+     *                  description="User password",
+     *                  type="string",
+     *              ),
+     *          ),
+     *      ),
+     *  ),
+     *   @OA\Response(
+     *       response=200,
+     *       description="User logged",
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *              @OA\Property(
+     *                  property="token",
+     *                  description="User email",
+     *                  type="string",
+     *                  example="1|0CyrUxPJMArqAqCDfqQklOebSDpv07aj7Kkf8HxW"
+     *              ),
+     *          ),
+     *      ),
+     *   )
+     * )
+     */
     public function login(LoginRequest $request)
     {
         $credentials = $this->credentials($request);
-        
+
         if (!Auth::attempt($credentials)) {
             return response([
                 'error' => 'Invalid credentials'
